7d45a53e9298d9e5b4b7aca46cbce28a7bfbcb9e
Alejandro Carcamo Martell <159275896+AlejandroCarcamoMartell@users.noreply.github.com>
2024-06-10 00:12:52 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+const charactersEl = document.getElementById('characters');
+const nameFilterEl = document.getElementById('name-filter');
+const statusFilterEl = document.getElementById('status-filter');
+
+//Funcion para la api
+
+async function getCharacters (name, status){
+
+    let url = 'https://rickandmortyapi.com/api/character/';
+
+    if (name || status){
+        url += '?';
+        if(name){
+            url += `name=${name}&`;
+        }
+
+        if(status){
+            url += `status=${status}`;
+        }
+    }
+
+
+    const response = await fetch(url);
+    const data = await response.json(); 
+
+    return data.results;
+
+}
+
+//Dom :)
+async function displayCharacters (name, status) {
+    
+    //filtrador
+    const characters = await getCharacters(name, status);
+    
+    charactersEl.innerHTML = '';
+
+    //renderizador
+    for( let character of characters ){
+        const card = document.createElement('div');
+        card.classList.add('character-card');
+
+        card.innerHTML = `
+            <img src="${character.image}" />
+            <h2> ${character.name} </h2>
+            <p> Status: ${character.status} </p>
+            <p> Especie: ${character.species} </p>
+        `;
+
+        charactersEl.appendChild(card);
+
+    }
+
+}
+
+displayCharacters();
+
+nameFilterEl.addEventListener('input', () => {
+    displayCharacters(nameFilterEl.value, statusFilterEl.value );
+});
+
+statusFilterEl.addEventListener('change', () => {
+    displayCharacters(nameFilterEl.value, statusFilterEl.value );
+});
